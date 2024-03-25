70a9b7cef617b83b2737ed6fa96364138680a653
William R <s80423@htw-dresden.de>
2024-03-25 10:16:35 +0100

    cleanup main.py

--- main.py
+++ main.py
@@ -23,28 +23,6 @@ def greedy():
         p.stop()
         p.open_in_browser()
 
-def PPO():
-    profile = False
-    start = time.time()
-    if profile:
-        from pyinstrument import Profiler
-
-        p = Profiler()
-        p.start()
-
-    rl_train()
-    print('---------------------------------------')
-    print()
-    #rl_test()
-
-    if profile:
-        p.stop()
-        p.open_in_browser()
-    end = time.time()
-    div = end - start
-    print("Time: " + str(div))
-
 
 if __name__ == '__main__':
-    #PPO()
     greedy()
